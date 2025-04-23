d0d3e3d72f11d3e8d26fe851a42bf974067a7613
typovrak <typovrak@gmail.com>
2025-04-24 01:14:48 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -5,4 +5,28 @@ let
 	group = config.users.users.${username}.group or "users";
 	home = config.users.users.${username}.home;
 in {
+	environment = {
+		systemPackages = with pkgs; [
+			dmenu
+			feh
+		];
+	};
+
+	fonts = {
+		enableDefaultPackages = true;
+		fontconfig.enable = true;
+		packages = with pkgs; [
+			(nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+		];
+	};
+
+	services.xserver = {
+		enable = true;
+		desktopManager.xterm.enable = true;
+		windowManager.i3.enable = true;
+		xkb = {
+			layout = "us";
+			variant = "altgr-intl";
+		};
+	};
 }
