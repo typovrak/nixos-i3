e1c217c7fcf13f857eb726053fb78d64f6b208fe
typovrak <typovrak@gmail.com>
2025-04-28 00:07:27 +0200

    add nvidia config

--- configuration.nix
+++ configuration.nix
@@ -53,5 +53,18 @@ in {
 			layout = "us";
 			variant = "altgr-intl";
 		};
+		videoDrivers = [ "nvidia" ];
+	};
+
+	hardware.nvidia = {
+		modesetting.enable = true;
+  		nvidiaSettings = true;
+  		powerManagement.enable = true;
+  		open = false;
+  		prime = {
+    		sync.enable = true;
+    		intelBusId = "PCI:0:2:0";
+    		nvidiaBusId = "PCI:1:0:0";
+  		};
 	};
 }
